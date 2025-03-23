439d886ad6d6533391d42eb21187e6e3c4446d73
yijing <n112140@gmail.com>
2025-03-23 16:10:04 +0800

    進入房間完成

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import { log } from "console";
 
 const app = express();
 const httpServer = createServer();
@@ -21,38 +22,67 @@ const io = new Server(httpServer, {
     },
 });
 
+let users = {};
 io.on("connection", (socket) => {
-    console.log(socket.id, " user connected");
+    socket.emit("sysmsg", `${socket.id}歡迎加入聊天室`);
+    socket.on("chatMessage", (msg) => {
+        console.log("message", msg);
+        socket.broadcast.emit("chatMessage", msg);
+    });
 
+    socket.on("set_username", (username) => {
+        users[username] = socket.id;
+    });
+    socket.on("join_room", (room) => {
+        console.log(`${socket.id} 進入房間: ${room}`);
+        socket.join(room); // 用戶加入房間
+        socket.emit("message", `你已經加入了房間: ${room}`);
+    });
+
+    // 發送訊息
+    socket.on('send_message', (messageData) => {
+        console.log("messageData", messageData);
+        const {sender_id,chat_id,message} = messageData;
+        if(chat_id){
+            io.to(chat_id).emit('receive_message', messageData);
+        }else{
+            if(users[sender_id]){
+                io.to(users[sender_id]).emit('receive_message', messageData);
+        }
+    }}) 
+    // 1 . 儲存到資料庫
+    // 2. 發送給所有在同一個房間的用戶
     // 監聽來自用戶的訊息
-    socket.on("message", (msg) => {
-        console.log("message: " + msg);
-        // 廣播訊息給所有連接的用戶
-        // io.emit("message", msg);
-        socket.broadcast.emit(
-            "message",
-            JSON.stringify({
-                user: socket.id,
-                message: msg,
-            })
-        );
-    });
-    socket.on("userid", (msg) => {
-        console.log("message: " + msg);
-        // 廣播訊息給所有連接的用戶
-        // io.emit("message", msg);
-        socket.broadcast.emit(
-            "message",
-            JSON.stringify({
-                user: socket.id,
-                message: msg,
-            })
-        );
-    });
-
+    // socket.on("message", (msg) => {
+    //     console.log("message: " + msg);
+    //     // 廣播訊息給所有連接的用戶
+    //     // io.emit("message", msg);
+    //     socket.broadcast.emit(
+    //         "message",
+    //         JSON.stringify({
+    //             user: socket.id,
+    //             message: msg,
+    //         })
+    //     );
+    // });
+    // socket.on("userid", (msg) => {
+    //     console.log("message: " + msg);
+    //     // 廣播訊息給所有連接的用戶
+    //     // io.emit("message", msg);
+    //     socket.broadcast.emit(
+    //         "message",
+    //         JSON.stringify({
+    //             user: socket.id,
+    //             message: msg,
+    //         })
+    //     );
+    // });
     // 當用戶斷開連接時
+    // socket.on("disconnect", () => {
+    //     console.log("user disconnected");
+    // });
     socket.on("disconnect", () => {
-        console.log("user disconnected");
+        console.log("user disconnected: " + socket.id);
     });
 });
 
